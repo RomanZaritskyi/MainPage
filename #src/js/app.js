9e723bac9a0c2e2ff682c69661257758cee024c0
Roman Zaritskyi <romanzaritskyi@gmail.com>
2020-10-14 22:24:47 +0300

    add #src

---
+++ #src/js/app.js
@@ -0,0 +1,2 @@
+@@include('files/slick.min.js', {})
+@@include('files/script.js', {})
---
+++ #src/js/files/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+
+    $('.carusel').slick({
+        mobileFirst: true,
+    })
+
+    $('.multiple-slider').slick({
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3,
+        speed: 900,
+        responsive: [
+            {
+                breakpoint: 1281,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '10px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+})
+
